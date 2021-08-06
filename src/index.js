6c889994356dbf672253452be88595257e294e85
Kthendavid04 <kthendavid04@gmail.com>
2021-08-06 13:04:56 -0500

    update file

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'materialize-css';
+import 'materialize-css/dist/css/materialize.min.css'
+
+ReactDOM.render(<App />, document.getElementById('root'));
